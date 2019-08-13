bf092cd8d772179d27617dc6cc8a5df0bad7a7ad
Zzl Zones <18296638944@163.com>
2019-08-13 16:50:46 +0800

    新增redis

--- src/test/java/xin/cymall/RabbitMQTest.java
+++ src/test/java/xin/cymall/RabbitMQTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import xin.cymall.common.mq.sender.Sender;
+import xin.cymall.common.utils.RedisUtil;
 
 /**
  * @Author zzl
@@ -17,9 +18,12 @@ import xin.cymall.common.mq.sender.Sender;
 public class RabbitMQTest {
     @Autowired
     private Sender sender;
-
+    @Autowired
+    private RedisUtil redisUtil;
     @Test
     public void testRabbitMQ(){
+        redisUtil.set("demo","123123");
         sender.send();
+
     }
 }
